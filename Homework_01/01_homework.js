dbcfc4e149eaa4034e92be051f00d2964e9a7ab1
Tatiana-goit <tanchou74@gmail.com>
2022-06-24 13:15:17 +0300

    solution of all tasks

--- Homework_01/01_homework.js
+++ Homework_01/01_homework.js
@@ -1,7 +1,6 @@
 // 1. Площа
 
 // Створіть веб-аплікацію, яка питає у користувача радіус кола та видає площу цього кола.
-
 // *Приклад:*
 // --> 123
 // <-- 47529.15525615998
@@ -12,29 +11,30 @@
 // const result = Math.PI * radius ** 2;
 // console.log(`Площадь круга с радиусом ${radius} = `, result);
 
+
+
 // 2. Пенсія
 
 // Створіть веб-аплікацію, яка питає у користувача його вік та виводить скільки йому залишилось до пенсії та в якому році вона настане.
-
 // Приклад:
 // --> 30
 // <-- 35, 2057
 // --> 1
 // <-- 64, 2086
 
-// const age = prompt('Введите свой возраст')
-// const resultAge = 65 - age
-// const date = new Date()
+// const PENSION = 65;
+// const date = new Date();
+// const age = prompt('Введите свой возраст');
+// const resultAge = PENSION - age;
 // // const year = 2022+result;
-// const year = date.getFullYear() + resultAge
-// console.log(
-//   `До пенсии вам осталось ${resultAge} лет и она начнется в ${year} году`,
-// )
+// const year = date.getFullYear() + resultAge;
+// console.log(`До пенсии вам осталось ${resultAge} лет и она начнется в ${year} году`)
+
+
 
 // 3. Навпаки
 
 // Запросіть у користувача трицифрове число і виведіть його задом наперед.
-
 // Приклад:
 // --> 321
 // <-- 123
@@ -47,24 +47,47 @@
 // const firstNumber = Math.floor(num / 100)
 // const secondNumber = Math.floor(num / 10) % 10
 // const lastNumber = num % 10
-// console.log(
-//   `Перевернутое число будет: ${lastNumber}${secondNumber}${firstNumber}`,
-// )
+// console.log(`Перевернутое число будет: ${lastNumber}${secondNumber}${firstNumber}`)
+
+
 
 // 2 вариант (с дополнительной проверкой по введенной информации)
 
-var num = 0
-while (true) {
-  num = prompt('Введите трехзначное число')
-  if ((num < 100 || num >= 1000) || (typeof(num)!==Number)){
-    alert('Введите корректное трехзначное число')
-  } else {
-    console.log(
-      `Перевернутое число будет: ${num % 10}${
-        Math.floor(num / 10) % 10
-      }${Math.floor(num / 100)}`,
-    )
-    break
-  }
-}
+// var num = 0
+// while (true) {
+//   num = prompt('Введите трехзначное число')
+//   if (num < 100 || num >= 1000) {
+//     alert('Введите корректное трехзначное число')
+//   } else {
+//     console.log(
+//       `Перевернутое число будет: ${num % 10}${
+//         Math.floor(num / 10) % 10
+//       }${Math.floor(num / 100)}`,
+//     )
+//     break
+//   }
+// }
 
+
+// 3 вариант перевернуть произвольное число через строку
+
+// const n = 12345678;
+// const str = n + '';
+// let newStr = '';
+
+// for (let i = str.length - 1; i >= 0; i = i - 1){
+//     newStr += (str[i]);
+// }
+// console.log('Для числа ', str,' перевернутое число будет: ', newStr);
+
+
+
+// 4 вариант перевернуть произвольное число через массив
+
+// const n = 12345678;
+// const str = String(n);
+// var arr = [];
+// for (let i = 0; i < str.length; i++) {
+//   arr.push(str[i]);
+// }
+// console.log(arr.reverse());
